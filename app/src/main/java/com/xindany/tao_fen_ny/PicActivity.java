2c8eeb983288e478629ae5ea2a95e6e6d8cfcde7
chenqiao <mrjoechen@qq.com>
2018-08-27 13:15:09 +0800

    矫正

--- app/src/main/java/com/xindany/tao_fen_ny/PicActivity.java
+++ app/src/main/java/com/xindany/tao_fen_ny/PicActivity.java
@@ -36,21 +36,45 @@ public class PicActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pic);
 
-        OpenCvUtil openCvUtil = new OpenCvUtil();
+
 
         imageView1 = findViewById(R.id.img1);
         imageView2 = findViewById(R.id.img2);
-        Bitmap bitmap = ((BitmapDrawable) getResources().getDrawable(
-                R.drawable.pic_demo)).getBitmap();
-        int w = bitmap.getWidth(), h = bitmap.getHeight();
-        int[] pix = new int[w * h];
-        bitmap.getPixels(pix, 0, w, 0, 0, w, h);
-        int [] resultPixes=openCvUtil.jiaozheng(pix,w,h);
-        Bitmap result = Bitmap.createBitmap(w,h, Bitmap.Config.RGB_565);
-        result.setPixels(resultPixes, 0, w, 0, 0,w, h);
+//        Bitmap bitmap = ((BitmapDrawable) getResources().getDrawable(
+//                R.drawable.pic_demo)).getBitmap();
+//        int w = bitmap.getWidth(), h = bitmap.getHeight();
+//        int[] pix = new int[w * h];
+//        bitmap.getPixels(pix, 0, w, 0, 0, w, h);
+//        int [] resultPixes=openCvUtil.jiaozheng(pix,w,h);
 
-        imageView2.setImageBitmap(result);
-        imageView1.setImageBitmap(bitmap);
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                final Bitmap bitmap = ((BitmapDrawable) getResources().getDrawable(
+                        R.drawable.pic_demo)).getBitmap();
+                final int w = bitmap.getWidth(), h = bitmap.getHeight();
+                int[] pix = new int[w * h];
+                OpenCvUtil openCvUtil = new OpenCvUtil();
+                final int [] resultPixes=openCvUtil.jiaozheng(pix,w,h);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Bitmap result = Bitmap.createBitmap(w,h, Bitmap.Config.RGB_565);
+                    result.setPixels(resultPixes, 0, w, 0, 0,w, h);
+
+                    imageView2.setImageBitmap(result);
+                    imageView1.setImageBitmap(bitmap);
+                    }
+                });
+
+            }
+        }.start();
+//        Bitmap result = Bitmap.createBitmap(w,h, Bitmap.Config.RGB_565);
+//        result.setPixels(resultPixes, 0, w, 0, 0,w, h);
+//
+//        imageView2.setImageBitmap(result);
+//        imageView1.setImageBitmap(bitmap);
 
 
     }
--- app/src/main/jni/com_xindany_OpenCvUtil.cpp
+++ app/src/main/jni/com_xindany_OpenCvUtil.cpp
@@ -3,6 +3,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+using namespace std;
+using namespace cv;
 
 /* Header for class com_xindany_OpenCvUtil */
 
@@ -21,36 +26,69 @@ extern "C" {
 JNIEXPORT jintArray JNICALL Java_com_xindany_OpenCvUtil_jiaozheng
   (JNIEnv *env, jobject obj, jintArray buf, int w, int h){
 
+Mat image = imread("/mnt/sdcard/in.jpg", 1);
     jint *cbuf;
       cbuf = env->GetIntArrayElements(buf, JNI_FALSE );
       if (cbuf == NULL) {
           return 0;
       }
 
-      cv::Mat imgData(h, w, CV_8UC4, (unsigned char *) cbuf);
+      Mat imgData(h, w, CV_8UC4, (unsigned char *) cbuf);
 
 
-    cv::Size image_size = imgData.size();
+
+    Size image_size = image.size();
     float intrinsic[3][3] = { 8.347918013266893e+02, 0, 6.367127655763162e+02,
     0, 8.292307453411623e+02, 3.603392433999688e+02,
     0, 0, 1 };
     float distortion[1][5] = { -0.318861538489663, 0.090963121959472, 0, 0, 0 };
-    cv::Mat intrinsic_matrix = cv::Mat(3, 3, CV_32FC1, intrinsic);
-    cv::Mat distortion_coeffs = cv::Mat(1, 5, CV_32FC1, distortion);
-    cv::Mat R = cv::Mat::eye(3, 3, CV_32F);	cv::Mat mapx = cv::Mat(image_size, CV_32FC1);
-    cv::Mat mapy = cv::Mat(image_size, CV_32FC1);
+    Mat intrinsic_matrix = Mat(3, 3, CV_32FC1, intrinsic);
+    Mat distortion_coeffs = Mat(1, 5, CV_32FC1, distortion);
+    Mat R = Mat::eye(3, 3, CV_32F);	Mat mapx = Mat(image_size, CV_32FC1);
+    Mat mapy = Mat(image_size, CV_32FC1);
     initUndistortRectifyMap(intrinsic_matrix, distortion_coeffs, R, getOptimalNewCameraMatrix(intrinsic_matrix, distortion_coeffs, image_size, 1, image_size, 0), image_size, CV_32FC1, mapx, mapy);
-    cv::Mat t = imgData.clone();
-    cv::remap(imgData, t, mapx, mapy, cv::INTER_LINEAR);
+    Mat t = image.clone();
+    remap(image, t, mapx, mapy, INTER_LINEAR);
 
     jint* ptr = (jint*)t.ptr(0);
 
+    imwrite("/mnt/sdcard/out.jpg", t);
 
       int size = w * h;
       jintArray result = env->NewIntArray(size);
       env->SetIntArrayRegion(result, 0, size, ptr);
       env->ReleaseIntArrayElements(buf, cbuf, 0);
       return result;
+
+
+/*
+
+        int size = w * h;
+      	jint * pinPixels = env->GetIntArrayElements(buf, 0);
+
+      	Mat in(h, w, CV_8UC4, (unsigned char*) pinPixels);
+        Mat out(h, w, CV_8UC4, Scalar(0, 0, 255));
+
+      	double cam[] = {w, 0, w / 2, 0, h, h / 2, 0, 0, 1 };
+      	double distort[] = { 0.1, 0.35, 0.0, 0.0, 0.01 };
+
+
+      	Mat camMat = Mat(3, 3, CV_64FC1, cam);
+      	Mat disMat = Mat(5, 1, CV_64FC1, distort);
+      	undistort(in, out, camMat, disMat);
+
+        jint* ptr = (jint*)out.ptr(0);
+
+        jintArray result = env->NewIntArray(size);
+
+        env->SetIntArrayRegion(result, 0, size, ptr);
+
+      	env->ReleaseIntArrayElements(buf, pinPixels, 0);
+
+      return result;
+      */
+
+
   }
 
   JNIEXPORT jintArray JNICALL Java_com_xindany_OpenCvUtil_grey
