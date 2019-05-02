5775da2cf7d76380e415aeea3934dd12764aa272
chenqiao <mrjoechen@qq.com>
2019-05-02 21:47:00 +0800

    自动拍照

---
+++ app/src/main/obj/local/armeabi-v7a/objs/NDKUtils/com_xindany_OpenCvUtil.o.d
@@ -0,0 +1,2 @@
+./obj/local/armeabi-v7a/objs/NDKUtils/com_xindany_OpenCvUtil.o: \
+  jni/com_xindany_OpenCvUtil.cpp
---
+++ app/src/main/obj/local/armeabi/objs/OpenCVUtil/com_xindany_OpenCvUtil.o.d
@@ -0,0 +1,2 @@
+./obj/local/armeabi/objs/OpenCvUtil/com_xindany_OpenCvUtil.o: \
+  jni/com_xindany_OpenCvUtil.cpp
